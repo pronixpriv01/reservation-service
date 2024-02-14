24b286691b4277ff28eaa2c2d4de0bb4ece6a39d
pronixpriv01 <simon-hunecker@outlook.de>
2024-02-14 11:25:02 +0100

    feat(cloudbuild): Add cloudbuild

--- libs/common/src/decorators/current-user.decorator.ts
+++ libs/common/src/decorators/current-user.decorator.ts
@@ -1,9 +1,10 @@
-import { createParamDecorator, ExecutionContext } from "@nestjs/common";
-import { UserDocument } from "../../../../apps/auth/src/users/models/user.schema";
+import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+import { UserDocument } from '@app/common/models';
 
 const getCurrentUserByContext = (context: ExecutionContext): UserDocument => {
   return context.switchToHttp().getRequest().user;
-}
+};
 export const CurrentUser = createParamDecorator(
-  (_data: unknown, context: ExecutionContext) => getCurrentUserByContext(context)
-)
+  (_data: unknown, context: ExecutionContext) =>
+    getCurrentUserByContext(context),
+);
